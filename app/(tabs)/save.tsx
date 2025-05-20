74e133ff3f891815d91ac3df86c2589ab1c73a58
Hazzembadran <hazemaqbadran@gmail.com>
2025-05-20 14:32:30 +0300

    feat: update profile and save components with improved layout and styling; enhance movie details fetching and add error handling; refactor TrendingCard and API service for better structure

--- app/(tabs)/save.tsx
+++ app/(tabs)/save.tsx
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { icons } from "@/constants/icons";
 
 const Save = () => {
   return (
-    <View>
-      <Text>Save</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex flex-col justify-center items-center flex-1 gap-5">
+        <Image source={icons.save} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-500 text-base">Save</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Save
+export default Save;
--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,7 +1,7 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
-import React from "react";
 import { Link } from "expo-router";
 import MaskedView from "@react-native-masked-view/masked-view";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
 import { images } from "@/constants/images";
 
 const TrendingCard = ({
@@ -16,16 +16,27 @@ const TrendingCard = ({
           className="w-32 h-48 rounded-lg"
           resizeMode="cover"
         />
-        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-lg">
-          <MaskedView maskElement={<Text className="font-bold text-white text-6xl">{index + 1}</Text>}>
-            <Image source={images.rankingGradient} 
-              className="size-14"  resizeMode="cover"
+
+        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+          <MaskedView
+            maskElement={
+              <Text className="font-bold text-white text-6xl">{index + 1}</Text>
+            }
+          >
+            <Image
+              source={images.rankingGradient}
+              className="size-14"
+              resizeMode="cover"
             />
-            <Text className="font-bold text-sm mt-2 text-light-200" numberOfLines={2}>
-              {title}
-            </Text>
           </MaskedView>
         </View>
+
+        <Text
+          className="text-sm font-bold mt-2 text-light-200"
+          numberOfLines={2}
+        >
+          {title}
+        </Text>
       </TouchableOpacity>
     </Link>
   );
